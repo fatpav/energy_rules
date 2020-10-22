b15d22bb3118f5ed6181590f0aa5f22b413db518
fatpav <fat_pav@hotmail.com>
2020-10-22 10:54:29 +0100

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <h1>What up, team!</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: 'app',
+  data() {
+    return {
+      natGrid: []
+    }
+  },
+  mounted() {
+    fetch('https://api.carbonintensity.org.uk/generation')
+    .then(response => response.json())
+    .then(natGrid => this.natGrid = natGrid)
   }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueGoogleCharts from 'vue-google-charts'
 
 Vue.config.productionTip = false
 
