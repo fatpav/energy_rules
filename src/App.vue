44ea0a6a4d70ba6bcef8ee4749ca8bab11ad843b
fatpav <fat_pav@hotmail.com>
2020-10-22 12:12:02 +0100

    made a chart

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,37 @@
 <template>
-  
+  <fuel-chart :chartData="chartData"></fuel-chart>  
 </template>
 
 <script>
+import FuelChart from './components/FuelChart.vue'
+
 export default {
   name: 'app',
+  
+
   data() {
     return {
-      natGrid: []
+      natGrid: [],
+      chartData: []
     }
   },
+
+  // computed: {
+  //   dataHandler: function() {
+  //     return natGrid.data.generationmix.map(pair => {
+  //      return chartData = [pair.fuel, pair.perc]
+    //   })
+    // }
+  // },
+
   mounted() {
     fetch('https://api.carbonintensity.org.uk/generation')
     .then(response => response.json())
     .then(natGrid => this.natGrid = natGrid)
+  },
+  components: {
+    "fuel-chart":FuelChart
   }
-
 }
 </script>
 
--- src/components/FuelChart.vue
+++ src/components/FuelChart.vue
@@ -1,11 +1,33 @@
 <template>
-    
+    <GChart
+        type="ColumnChart"
+        :data="chartData"
+        :options="chartOptions"
+        />
 </template>
 
 <script>
-import { GChart } from 'vue-google-charts',
+import { GChart } from 'vue-google-charts'
 
 export default {
+    data() {
+        return {
+        chartData: [
+            ['fuel', 'type'],
+            ['nuclear', 20.4],
+            ['hamster', 15],
+            ['farts', 80]
+        ],
+        chartOptions: {
+            chart: {
+                title: 'Please please work',
+
+            }
+        }
+    }
+    
+    },
+
     name: 'fuel-chart',
     props: ['natGrid'],
 
@@ -15,6 +37,8 @@ export default {
     }
 
 }
+
+
 </script>
 
 <style>
